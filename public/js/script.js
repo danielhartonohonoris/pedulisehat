812316ef2efd896153fb1edb647ea5d180044eaf
danielhartonohonoris <daniel.535220206@stu.untar.ac.id>
2024-04-05 09:23:56 +0700

    mengubah tampilan

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,4 @@
-'use strict';
-
-
+"use strict";
 
 /**
  * add event listener on multiple elements
@@ -10,13 +8,11 @@ const addEventOnElements = function (elements, eventType, callback) {
   for (let i = 0, len = elements.length; i < len; i++) {
     elements[i].addEventListener(eventType, callback);
   }
-}
-
-
+};
 
 /**
  * PRELOADER
- * 
+ *
  * preloader will be visible until document load
  */
 
@@ -27,11 +23,9 @@ window.addEventListener("load", function () {
   document.body.classList.add("loaded");
 });
 
-
-
 /**
  * MOBILE NAVBAR
- * 
+ *
  * show the mobile navbar when click menu button
  * and hidden after click menu close button or overlay
  */
@@ -44,15 +38,13 @@ const toggleNav = function () {
   navbar.classList.toggle("active");
   overlay.classList.toggle("active");
   document.body.classList.toggle("nav-active");
-}
+};
 
 addEventOnElements(navTogglers, "click", toggleNav);
 
-
-
 /**
  * HEADER & BACK TOP BTN
- * 
+ *
  * active header & back top btn when window scroll down to 100px
  */
 
@@ -67,11 +59,9 @@ const activeElementOnScroll = function () {
     header.classList.remove("active");
     backTopBtn.classList.remove("active");
   }
-}
+};
 
 window.addEventListener("scroll", activeElementOnScroll);
-
-
 
 /**
  * SCROLL REVEAL
@@ -81,13 +71,16 @@ const revealElements = document.querySelectorAll("[data-reveal]");
 
 const revealElementOnScroll = function () {
   for (let i = 0, len = revealElements.length; i < len; i++) {
-    if (revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.15) {
+    if (
+      revealElements[i].getBoundingClientRect().top <
+      window.innerHeight / 1.15
+    ) {
       revealElements[i].classList.add("revealed");
     } else {
       revealElements[i].classList.remove("revealed");
     }
   }
-}
+};
 
 window.addEventListener("scroll", revealElementOnScroll);
 
