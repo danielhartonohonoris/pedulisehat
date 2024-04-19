55f60c07509f227c9ab6e31b7693846e8bc81066
amosnbbn <amosvalentino95@gmail.com>
2024-04-19 20:41:21 +0700

    wkwkw

---
+++ models/DaftarPenyakit.js
@@ -0,0 +1,31 @@
+const {Schema, model} = require('mongoose');
+
+const TodoListItemsSchema = new Schema({
+    title: {
+        type: 'string',
+        required: true
+    },
+    completed: {
+        type: 'boolean',
+        default: false
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+
+
+module.exports = model('TodolistSickness', TodoListItemsSchema);
+
+
+
