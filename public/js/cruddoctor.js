ad6c6f2ccca06d16aa10f543a4f72401e74a75fb
Dyraaa18 <adang.535220192@stu.untar.ac.id>
2024-05-04 14:37:21 +0700

    nambahin komen doang

--- public/js/cruddoctor.js
+++ public/js/cruddoctor.js
@@ -1,3 +1,4 @@
+// Fungsi untuk menampilkan atau menyembunyikan formulir edit
 function showEditForm(doctorId) {
     var editFormContainer = document.getElementById('editFormContainer_' + doctorId);
     if (editFormContainer.style.display === 'none') {
@@ -11,31 +12,12 @@ function showEditForm(doctorId) {
         editFormContainer.style.display = 'none';
     }
 }
+// Fungsi untuk membatalkan edit formulir
 function cancelEditForm(doctorId) {
     var editFormContainer = document.getElementById('editFormContainer_' + doctorId);
     editFormContainer.style.display = 'none';
 }
-
-function submitEditForm(doctorId) {
-    var form = document.getElementById("editForm_" + doctorId);
-    var formData = new FormData(form);
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", form.action, true);
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            console.log("Perubahan berhasil disimpan!");
-            window.location.reload();
-        } else {
-            console.error("Terjadi kesalahan saat menyimpan perubahan.");
-        }
-    };
-    xhr.onerror = function () {
-        console.error("Terjadi kesalahan koneksi.");
-    };
-    xhr.send(formData);
-}
-
+// Fungsi untuk menambahkan dokter ke server
 function submitFormDoctor() {
     var form = document.getElementById("doctorForm");
     var formData = new FormData(form);
@@ -55,7 +37,7 @@ function submitFormDoctor() {
     };
     xhr.send(formData);
 }
-
+// Fungsi untuk mengupdate dokter ke server menggunakan metode PUT.
 function submitUpdateForm() {
     var form = document.getElementById("updateForm");
     var formData = new FormData(form);
@@ -75,7 +57,7 @@ function submitUpdateForm() {
     };
     xhr.send(formData);
 }
-
+// Fungsi untuk menghapus data dokter dari server
 function deletedoctors(doctorid) {
     if (confirm("Apakah Anda yakin ingin menghapus dokter ini?")) {
         var xhr = new XMLHttpRequest();
